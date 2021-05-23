e894e86247e809e11c826f57e27b8a40c9bcc156
David K Hite <david.k.hite@gmail.com>
2021-05-22 23:04:54 -0700

    Added express server with route to geocode API

---
+++ config/prod.js
@@ -0,0 +1,6 @@
+// prod.js
+
+module.exports = {
+    OWMAppId: process.env.OWM_APP_ID,
+    openCageApiKey: process.env.OPEN_CAGE_API_KEY
+};
---
+++ routes/geocodeRoutes.js
@@ -0,0 +1,43 @@
+const fetch = require('node-fetch');
+const keys = require('../config/keys');
+
+
+module.exports = app => {
+    app.get('/api/geocode', async ({ query }, res) => {
+        console.log(query);
+        if (!query.city || !query.state) {
+            res.send("Missing parameter");
+            return;
+        }
+
+        const geocodeUrl = //'https://cors-anywhere.herokuapp.com/'
+                            'https://api.opencagedata.com/geocode/v1/json?q='
+                            + query.city
+                            + '%2C%20'
+                            + query.state
+                            + '&key=' + keys.openCageApiKey
+                            + '&language=en&pretty=1&no_annotations=1';
+        console.log(geocodeUrl);
+        const response = await fetch(geocodeUrl, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }).catch(console.log);
+
+        const data = await response.json().catch(console.log);
+        const coords = getCoordinatesFromData(data);
+        console.log(coords);
+        res.send(coords);
+    });
+};
+
+const getCoordinatesFromData = (data) => {
+    const bounds = data.results[0].bounds;
+    const latf = (parseFloat(bounds.northeast.lat) + parseFloat(bounds.southwest.lat)) / 2.0;
+    const lngf = (parseFloat(bounds.northeast.lng) + parseFloat(bounds.southwest.lng)) / 2.0;
+    return {
+        lat: latf,
+        lon: lngf
+    };
+};
