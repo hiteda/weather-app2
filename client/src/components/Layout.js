caa9ffedf3cf31ec140dfcde99517c643a48a814
David K Hite <david.k.hite@gmail.com>
2021-05-28 21:56:50 -0700

    Added new landing page

--- client/src/components/Layout.js
+++ client/src/components/Layout.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Header from './Header/Header';
 import ForecastList from './Forecast/ForecastList';
+import Landing from './Landing/Landing';
 
 class Layout extends Component {
     state = {}
@@ -22,6 +23,7 @@ class Layout extends Component {
                 <Header />
                 <div className="container">
                     { this.state.forecasts && this.state.forecasts.length > 0 && <ForecastList forecasts={this.state.forecasts} /> }
+                    { (!this.state.forecasts || this.state.forecasts.length < 1) && <Landing /> }
                     <div className="fixed-action-btn">
                         <button onClick={this.handleClicked} className='btn-floating pulse waves-effect waves-light large cyan darken-3'>
                             <i className='material-icons'>add</i>
