a5b6c4a5842648af25d48f29bd4118682eea9bad
David K Hite <david.k.hite@gmail.com>
2021-05-28 21:33:47 -0700

    Serve production assets

--- index.js
+++ index.js
@@ -5,5 +5,18 @@ const app = express();
 require('./routes/geocodeRoutes')(app);
 require('./routes/weatherRoutes')(app);
 
+if (process.env.NODE_ENV === 'production') {
+    // Make sure express serves production assets (i.e.
+    // main.js, main.css)
+    app.use(express.static('client/build'));
+
+    // Make express serve up index.html if it doesn't
+    // recognize the route
+    const path = require('path');
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
